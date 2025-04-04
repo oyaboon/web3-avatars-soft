d1770c740afa746e50403d511a3838a51a2eb9e0
Owlhoot <floome@outlook.com>
2025-04-04 21:56:42 +0400

    //

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'demo',
+    outDir: resolve(__dirname, 'demo'),
     emptyOutDir: true,
   },
   root: 'src/demo',
